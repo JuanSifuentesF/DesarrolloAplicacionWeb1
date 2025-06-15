d9f7e75e74a97f45bdc26b0d9704bd5ee30246dd
Juan Sifuentes Fernandez <89866333+JuanSifuentesF@users.noreply.github.com>
2025-06-14 23:12:34 -0500

    test

---
+++ spring-data-jpa-01/src/main/java/pe/edu/cibertec/springdatajpa01/SpringDataJpa01Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.springdatajpa01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpa01Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringDataJpa01Application.class, args);
+  }
+
+}
---
+++ spring-data-jpa-01/src/test/java/pe/edu/cibertec/springdatajpa01/SpringDataJpa01ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.springdatajpa01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpa01ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
